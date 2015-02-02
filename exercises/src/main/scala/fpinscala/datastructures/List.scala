774ade788223b625319f66c7aa5f935ee3ca0264
Dan Clark <dan.clark@quanttus.com>
2015-02-02 09:09:07 -0500

    Answer to tail function before looking at answer

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -8,7 +8,7 @@ object List { // `List` companion object. Contains functions for creating and wo
   def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
     case Nil => 0 // The sum of the empty list is 0.
     case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
-  } 
+  }
   
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
@@ -47,7 +47,11 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, Nil) => Nil
+    case Cons(h, t) => t
+  }
 
   def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
 
