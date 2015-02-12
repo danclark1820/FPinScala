1d724a120d91ea5a57a313af59f9c7c60cac81fa
Dan Clark <dan.clark@quanttus.com>
2015-02-12 14:41:31 -0500

    fold and previous excercises via fold

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -24,6 +24,26 @@ object Tree {
     case Leaf(_) => 0
     case Branch(l, r) => 1 + (depth(l) max depth(r))
   }
-  
 
+  def map[A,B](t:Tree[A])(f: A => B):Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A,B](t:Tree[A])(f:A => B)(g:(B, B) => B):B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g)) 
+  }
+
+  def sizeViaFold[A](t:Tree[A]):Int =
+    fold(t)(a => 1)(1 + _ + _)
+
+  def countLeaves[A](t:Tree[A]):Int =
+    fold(t)(a => 1)(_ + _)
+
+  def maxViaFold[A](t:Tree):Int =
+    fold(t)(a => a)(_ max _)
+
+  def depthViaFold[A](t:Tree):Int =
+    fold(t)(a => 0)((d1, d2) => 1 + (d1 max d2))
 }
